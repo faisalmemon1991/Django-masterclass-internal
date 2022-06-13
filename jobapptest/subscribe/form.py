19e6b9f08625412b8dc9c051a7fa032fed33f4eb
Faisal Memon <faisalmemon@hello.com>
2022-06-13 08:45:05 +0530

    ModelForms configured

--- jobapptest/subscribe/form.py
+++ jobapptest/subscribe/form.py
@@ -1,10 +1,30 @@
 from django import forms
 
+from subscribe.models import Subscribe
 
-class SubscribeForm(forms.Form):
-    # name = forms.CharField(label='Your name', max_length=100)
-    # email = forms.EmailField(label='Your email', max_length=100)
+class SubscribeForm(forms.ModelForm):
+    class Meta:
+        model=Subscribe
+        fields = '__all__'
 
-    first_name = forms.CharField(max_length=100)
-    last_name = forms.CharField(max_length=100)
-    email = forms.EmailField(max_length=100)
+# def validate_comma(value):
+#     if "," in value:
+#         raise forms.ValidationError("Invalid Last Name")
+#     return value
+
+# class SubscribeForm(forms.Form):
+#     # name = forms.CharField(label='Your name', max_length=100)
+#     # email = forms.EmailField(label='Your email', max_length=100)
+
+#     first_name = forms.CharField(max_length=100, required=False, label="Enter first name", help_text="Enter characters only")
+#     last_name = forms.CharField(max_length=100, validators=[validate_comma])
+#     email = forms.EmailField(max_length=100)
+
+#     def clean_first_name(self):
+#         data = self.cleaned_data['first_name']
+#         if "," in data:
+#             raise forms.ValidationError("Invalid First Name")
+#         return data
+
+
+
