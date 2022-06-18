b2552932c063ade1ca7573435ed6df8adbdcac32
Faisal Memon <faisalmemon@hello.com>
2022-06-18 16:33:11 +0530

    saving modelforms

--- jobapptest/subscribe/views.py
+++ jobapptest/subscribe/views.py
@@ -16,17 +16,17 @@ def subscribe(request):
         subscribe_form = SubscribeForm(request.POST)
         if subscribe_form.is_valid():
             print("valid form")
-            # subscribe_form.save()
-            email = subscribe_form.cleaned_data['email']
-            first_name = subscribe_form.cleaned_data['first_name']
-            last_name = subscribe_form.cleaned_data['last_name']
-            print(email)
+            subscribe_form.save()
+            # email = subscribe_form.cleaned_data['email']
+            # first_name = subscribe_form.cleaned_data['first_name']
+            # last_name = subscribe_form.cleaned_data['last_name']
+            # print(email)
         # if email=="":
         #     email_error_empty = "No email entered";
 
-            subscribe = Subscribe(first_name = first_name, last_name = last_name, email = email)
-            subscribe.save()
-            print(f"{subscribe.first_name} - {subscribe.email}")
+            # subscribe = Subscribe(first_name = first_name, last_name = last_name, email = email)
+            # subscribe.save()
+            # print(f"{subscribe.first_name} - {subscribe.email}")
             return redirect(reverse('thank_you'))
     context={"form":subscribe_form}
     return render(request,"subscribe/subscribe.html",context)
