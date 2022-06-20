22381b38e7c2f6eb0d1bb6589111b042038068b7
Faisal Memon <faisalmemon@hello.com>
2022-06-20 22:59:25 +0530

    same file css and js added

--- jobapptest/subscribe/models.py
+++ jobapptest/subscribe/models.py
@@ -1,7 +1,21 @@
+from re import M
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.core.validators import EmailValidator
+
+def validate_comma(value):
+    if "," in value:
+        raise ValidationError("Invalid Last Name, it contains comma")
+    return value
+
+NEWSLETTER_OPTION = [
+    ('W', 'Weekly'),
+    ('M', 'Monthly'),
+]
 
 # Create your models here.
 class Subscribe(models.Model):
     first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    email = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100, validators=[validate_comma])
+    email = models.EmailField(max_length=100)
+    option = models.CharField(max_length=2, default='M', choices=NEWSLETTER_OPTION)
