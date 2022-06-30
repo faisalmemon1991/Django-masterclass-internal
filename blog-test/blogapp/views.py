77fbbfac2d07206a44c0ddb3beb600be27c7da4f
Faisal Memon <faisalmemon@hello.com>
2022-07-01 00:29:45 +0530

    blog home page setup

--- blog-test/blogapp/views.py
+++ blog-test/blogapp/views.py
@@ -3,5 +3,9 @@ from blogapp.models import Post
 
 # Create your views here.
 def index(request):
-    blogs = Post.objects.all()
-    return render(request, 'blogapp/index.html', {'blogs': blogs})  
+    top_blogs = Post.objects.all().order_by('-view_count')
+    recent_blogs = Post.objects.all().order_by('-last_updated')
+
+    context = {'top_blogs': top_blogs, 
+                'recent_blogs': recent_blogs}
+    return render(request, 'blogapp/index.html', context)  
